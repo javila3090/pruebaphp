a0060692a2cd0d15022c1b064e58f531bdddff9d
Julio <javila3090@gmail.com>
2017-02-24 17:58:34 -0400

    Proyecto PHP con laravel 5.3 - App lista

--- resources/views/valorarServicio.blade.php
+++ resources/views/valorarServicio.blade.php
@@ -1,8 +1,34 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+@extends('app')
+    
+@section('content')
+<div class="container">
+    <ol class="breadcrumb" style="font-size: 12px;">
+        <span><b>Est&aacute;s en:</b></span>
+        <li><a href="{{ route('/') }}"><b>Inicio</b></a></li>
+        <li class="active">Servicios</li>        
+    </ol>
+    <form id="registro" role="form">
+        <div class="jumbotron myBox"> 
+            <div class="container">
+                <div class="col-lg-4 callout centered">
+                    <span class="fa fa-html5 fa-5x"></span>
+                    <h2>Desarrollo Web</h2>
+                    <hr>
+                </div><!-- col-lg-4 -->
+                    
+                <div class="col-lg-4 callout centered">
+                    <span class="fa fa-mobile fa-5x"></span>
+                    <h2>Diseño responsivo</h2>
+                    <hr>
+                </div><!-- col-lg-4 -->	
+                    
+                <div class="col-lg-4 callout centered">
+                    <span class="fa fa-lightbulb-o fa-5x"></span>
+                    <h2>Trabajo creativo</h2>
+                    <hr>
+                </div><!-- col-lg-4 -->	
+            </div><!-- row -->
+        </div>
+    </form>
+</div>
+@endsection
